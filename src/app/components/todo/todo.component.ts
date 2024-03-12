4d79487de8ddf37ded08e3f318a3268899affac7
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-11 19:19:26 -0500

    actualizar components

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilterType, TodoModel } from '../../models/todo';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  todolist = signal<TodoModel[]>([
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+      editing: false
+    },
+    {
+      id: 2,
+      title: 'Learn React',
+      completed: false,
+      editing: false
+    },
+    {
+      id: 3,
+      title: 'Learn Vue',
+      completed: false,
+      editing: false
+    },
+  ]);
 
+  filter = signal<FilterType>('all');
+
+  newTodo = new FormControl('asasdadsdasdad', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(3)]
+  })
+
+  changeFilter(filterString: FilterType){
+    this.filter.set(filterString);
+  }
 }
